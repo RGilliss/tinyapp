de72c7d07a26e3f786fd5a1fdcd5837c1fd9839d
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-15 23:46:02 +0000

    body parser isntalled

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +10,10 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
