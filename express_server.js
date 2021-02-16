1551b91f2a76bfbb3113d3ba1499e716fb0ca18f
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-16 21:27:15 +0000

    edit button and page added with minimal fucntionality

--- express_server.js
+++ express_server.js
@@ -2,17 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set("view engine", "ejs");
-
-
-
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let shortened = "";
+  const alphanum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  for (let i = 0; i <= 5; i++) {
+    shortened += alphanum.charAt(Math.floor(Math.random() * alphanum.length));
+  }
+  return shortened;
+}
+
+//
+// MIDDLEWARE
+//
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+//
+// GET
+//
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -46,6 +60,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//
+//  POST
+//
 
 app.post("/urls", (req, res) => {
   let newShortURL = generateRandomString();
@@ -60,14 +77,23 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-function generateRandomString() {
-  let shortened = "";
-  const alphanum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  for (let i = 0; i <= 5; i++) {
-    shortened += alphanum.charAt(Math.floor(Math.random() * alphanum.length));
+app.post("/urls_show", (req, res) => { 
+  const {longURL} = req.body;
+  const newKey = generateRandomString()
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL] === longURL) {
+      urlDatabase[shortURL] = urlDatabase[newKey];
+      urlDatabase[newKey] = longURL;
+      delete urlDatabase[shortURL];
+    };
   }
-  return shortened;
-}
+  res.redirect("/urls");
+});
+
+app.post('/urls/:shortURL/edit', (req, res) => {
+  res.redirect('/urls/:shortURL')
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
