045e65cdf6e920fe278d44a3bb7718aad87e27cf
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-17 20:14:18 +0000

    Cleaned up code

--- express_server.js
+++ express_server.js
@@ -8,9 +8,7 @@ const cookieParser = require('cookie-parser')
 //
 
 app.use(cookieParser());
-
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
 
 //
@@ -31,10 +29,24 @@ function generateRandomString() {
   return shortened;
 }
 
+//
+// GET
+//
 
-//
-// CREATE
-//
+//Redirect to /urls if logged in or /login if loged in
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+//Render urls_index page (/urls) with templateVars in urls_index
+app.get("/urls", (req, res) => {
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies['username'],
+  };
+  res.render("urls_index", templateVars);
+});
+
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
@@ -43,14 +55,6 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
-//Initialize templateVars in urls_index
-app.get("/urls", (req, res) => {
-  const templateVars = { 
-    urls: urlDatabase,
-    username: req.cookies['username'],
-  };
-  res.render("urls_index", templateVars);
-});
 
 //Initialize templateVars in urls_show
 app.get("/urls/:shortURL", (req, res) => {
@@ -71,6 +75,7 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 //Redirect to longURL
+//NEED TO FIX
 // app.get("/u/:shortURL", (req, res) => {
 //   const longURL = req.params;
 //   res.redirect(longURL);
@@ -80,9 +85,6 @@ app.get("/urls/:shortURL", (req, res) => {
 //Part of Initial Set Up
 //
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -92,14 +94,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-//
-// READ
-//
-
-
 
 //
-//  UPDATE
+// POST
 //
 
 //Generates intial shortURL
@@ -115,6 +112,13 @@ app.post("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls`);
+});
+
+//delete
+app.post("/urls/:shortURL/delete", (req, res) => { 
+  const {shortURL} = req.params;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 });
 
 //Redirects from edit to :shortURL
@@ -134,15 +138,6 @@ app.post('/logout', (req, res) => {
   res.redirect(`/urls`)
 });
 
-//
-// DELETE
-//
-
-app.post("/urls/:shortURL/delete", (req, res) => { 
-  const {shortURL} = req.params;
-  delete urlDatabase[shortURL];
-  res.redirect("/urls");
-});
 
 
 app.listen(PORT, () => {
